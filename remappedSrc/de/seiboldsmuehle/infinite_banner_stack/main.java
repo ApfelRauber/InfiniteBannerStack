6b546e87cd39aeada908436cf88831ee266600d5
AJ <adrian@seiboldsmuehle.de>
2023-02-16 19:55:32 +0100

    V1.0 done

--- remappedSrc/de/seiboldsmuehle/infinite_banner_stack/main.java
+++
@@ -1,9 +0,0 @@
-package de.seiboldsmuehle.infinite_banner_stack;
-
-import net.fabricmc.api.ModInitializer;
-
-public class main implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    }
-}
--- remappedSrc/de/seiboldsmuehle/infinite_banner_stack/mixin/MixinLoomScreenHandler.java
+++
@@ -1,7 +0,0 @@
-package de.seiboldsmuehle.infinite_banner_stack.mixin;
-
-
-
-
-public class MixinLoomScreenHandler {
-}
